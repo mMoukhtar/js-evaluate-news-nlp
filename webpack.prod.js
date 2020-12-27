4203e9959ff7a463dc65b300b90e0fb9070852d5
mMoukhtar <mohamed.moukhtar@outlook.com>
2020-12-27 23:27:48 +0200

    add workboxPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: ['@babel/polyfill', './src/client/index.js'],
@@ -42,6 +43,7 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css',
         }),
+        new WorkboxPlugin.GenerateSW(),
     ],
     output: {
         libraryTarget: 'var',
